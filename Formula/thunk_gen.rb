90c9004e6d66ee7cbf3c595308b65765b17c563a
Piper McCorkle <contact@piperswe.me>
2025-09-30 21:54:45 +0000

    add thunk_gen

---
+++ Formula/thunk_gen.rb
@@ -0,0 +1,19 @@
+class ThunkGen < Formula
+  desc "thunk generator for C and assembler"
+  homepage "https://github.com/stsp/thunk_gen"
+  url "https://github.com/stsp/thunk_gen/archive/refs/tags/1.6.tar.gz"
+  sha256 "d9252429b9998d8ccc68eaafa3d6504d4a9f47dc8068a841c4084658d0f2c9cf"
+  license "GPL-3.0"
+
+  depends_on "meson" => :build
+
+  def install
+    system "meson", "setup", "build", *std_meson_args
+    system "meson", "compile", "-C", "build"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    system "true"
+  end
+end
