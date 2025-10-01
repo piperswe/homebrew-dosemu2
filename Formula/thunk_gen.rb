ca8476e3a2f96f257fc3cc810ce4ff8fac9b0732
Piper McCorkle <contact@piperswe.me>
2025-10-01 01:11:29 +0000

    thunk_gen: add 1.6 bottle.

--- Formula/thunk_gen.rb
+++ Formula/thunk_gen.rb
@@ -5,6 +5,13 @@ class ThunkGen < Formula
   sha256 "d9252429b9998d8ccc68eaafa3d6504d4a9f47dc8068a841c4084658d0f2c9cf"
   license "GPL-3.0"
 
+  bottle do
+    root_url "https://github.com/piperswe/homebrew-dosemu2/releases/download/thunk_gen-1.6"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "27b0a02b3f5683a009d231c6d8e651f6e03ee21a6b3b447fac43a3ff8170ad07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "36a28ed7c67a8e025388435cec17ee65ef98ea08a478611ffec34a02574d9707"
+  end
+
   depends_on "bison" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
